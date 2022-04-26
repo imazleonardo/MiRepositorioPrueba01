815d60e6f33a7a786831c748fa861cc54a792812
imazleonardo <104107470+imazleonardo@users.noreply.github.com>
2022-04-25 21:08:26 -0300

    Clase2022-04-25_ejerc01

---
+++ src/datos.ts
@@ -0,0 +1,9 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.forms[0].dato;
+let rotulo = document.getElementById("rotulo");
+
+btnEnv.addEventListener("click", () => {
+  console.log("La altura es: ", dato.value);
+});
+
+rotulo.innerHTML = "Indique la altura de la persona: ";
